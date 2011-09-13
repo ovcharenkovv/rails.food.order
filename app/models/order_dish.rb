bae52a7d3ae57cbdead2f42d5a102ab6093eca96
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2011-09-13 19:09:48 +0300

    add ordering to dises_list

--- app/models/order_dish.rb
+++ app/models/order_dish.rb
@@ -3,12 +3,21 @@ class OrderDish < ActiveRecord::Base
   belongs_to :dish
 
   def self.count_order_dishes
-    select("dish_id, COUNT(*) AS cnt").group('dish_id').where('DATE(order_dishes.created_at) = ?', Date.today)
+    find(:all,
+         :joins => "LEFT JOIN dishes on order_dishes.dish_id = dishes.id" ,
+         :select => "dish_id, COUNT(*) AS cnt",
+         :group =>"order_dishes.dish_id",
+         :conditions => ["DATE(order_dishes.created_at) = ?", Date.today],
+         :order => "dishes.category_id"
+    )
+
+
+    #select("dish_id, COUNT(*) AS cnt").group('dish_id').where('DATE(order_dishes.created_at) = ?', Date.today)
 
     #find_by_sql("SELECT *, COUNT(*) AS cnt FROM order_dishes  od WHERE DATE(created_at) = '#{Date.today}' GROUP BY dish_id;")
-#    today =Time.now.midnight .. (Time.now.midnight + 1.day)
-#    joins(:orders).where(:orders => {:created_at => today})
-#    find(:all, :select => "*, COUNT(*) AS cnt", :where=>"DATE(created_at) = \"#{Date.today}\"", :group => "order_dishes.dish_id")
+    #today =Time.now.midnight .. (Time.now.midnight + 1.day)
+    #joins(:orders).where(:orders => {:created_at => today})
+    #find(:all, :select => "*, COUNT(*) AS cnt", :where=>"DATE(created_at) = \"#{Date.today}\"", :group => "order_dishes.dish_id")
   end
 
   def self.today_orders
