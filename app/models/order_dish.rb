447c80d056173c88b4b2b5e73b94a1bc581e175c
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2011-09-13 11:03:36 +0300

    fix

--- app/models/order_dish.rb
+++ app/models/order_dish.rb
@@ -3,7 +3,7 @@ class OrderDish < ActiveRecord::Base
   belongs_to :dish
 
   def self.count_order_dishes
-    find_by_sql("SELECT *, COUNT(*) AS cnt FROM order_dishes  od WHERE DATE(created_at) = '#{Date.today}' GROUP BY dish_id;")
+    find_by_sql('SELECT *, COUNT(*) AS cnt FROM order_dishes  od WHERE DATE(created_at) = "#{Date.today}" GROUP BY dish_id;')
 #    today =Time.now.midnight .. (Time.now.midnight + 1.day)
 #    joins(:orders).where(:orders => {:created_at => today})
 #    find(:all, :select => "*, COUNT(*) AS cnt", :where=>"DATE(created_at) = \"#{Date.today}\"", :group => "order_dishes.dish_id")
